deabdadf38c891d33a00d1c8675e6a98520740ee
Rafael Sandoval <sandoval.rafael@gmail.com>
2019-04-29 20:23:42 +0200

    Se modifican URLS.PY y VIEWS.PY y se agrega testBusqCategoria

--- pagweb/views.py
+++ pagweb/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def busq_categoria(request):
+    lista_resultados = ("Fotografo1","Fotografo2","Fotografo3")
+    resultados = {"lista_resultados": lista_resultados}
+    #lista_resultado = Freelancer.objects.filter(categoria=request.GET[categoria])
+    return render(request, 'testBusqCategoria.html', resultados)
