f592ee13dbc7671c397eb62292e35f62d944be8d
Rafael Sandoval <sandoval.rafael@gmail.com>
2019-04-29 21:34:14 +0200

    Se cambia la funcion busq_categoria de views.py y se agrega el index.html a pagweb/templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from pagweb import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('testCategorias/', views.busq_categoria, name='testCategorias'),
 ]
--- pagweb/views.py
+++ pagweb/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    url_homepage = "index.html"
+    return render(request, url_homepage)
+
+
 def busq_categoria(request):
-    lista_resultados = ("Fotografo1","Fotografo2","Fotografo3")
-    resultados = {"lista_resultados": lista_resultados}
+    #lista_resultados = ("Fotografo1","Fotografo2","Fotografo3")
+    #resultados = {"lista_resultados": lista_resultados}
+    
     #lista_resultado = Freelancer.objects.filter(categoria=request.GET[categoria])
+    
+    lista_freelancers = Freelancer.objects.all()
+    contexto = {"lista_t": lista_freelancers}
+    
     return render(request, 'testBusqCategoria.html', resultados)
+
