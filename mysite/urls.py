bbad75cec8aa4ad5c37be393886b4aedbbaf852e
Rafael Sandoval <sandoval.rafael@gmail.com>
2019-04-30 20:12:24 +0200

    Se elimino un corchete de mas

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('testCat/', views.busq_categoria, name='testCat'),
     path('crearfree/', views.crear_freelancer),
     path('test_detalle/', views.desplegar_detalle, name='test_detalle'),
-]
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
