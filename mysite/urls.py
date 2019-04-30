0977d719d668651076e779ec4ac5bbf29b09ddaa
Rafael Sandoval <sandoval.rafael@gmail.com>
2019-04-30 17:56:53 +0200

    "Codigo de testCat"

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pagweb import views
-#from . import settings
-#from django.contrib.staticfiles.urls import static
-#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
- 
+from django.conf import settings
+from django.conf.urls.static import static
+
 #urlpatterns += staticfiles_urlpatterns()
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
@@ -27,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('testCat/', views.busq_categoria, name='testCat'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
