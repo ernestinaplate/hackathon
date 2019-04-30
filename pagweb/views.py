ea90ba3de603952edb172dde3654b844f1d82034
Rafael Sandoval <sandoval.rafael@gmail.com>
2019-04-30 17:46:59 +0200

    Se agregaron cambios en views y testCat

--- pagweb/views.py
+++ pagweb/views.py
@@ -9,18 +9,12 @@ def index(request):
 
 
 def busq_categoria(request):
-    #lista_resultados = ("Fotografo1","Fotografo2","Fotografo3")
-    #resultados = {"lista_resultados": lista_resultados}
-    
     ####### Prueba GET y objects.filter()
     lista_resultado = Freelancer.objects.filter(profesion=request.GET["c"])
-    #######
 
     ####### Prueba objects.all()
     #lista_freelancers = Freelancer.objects.all()
-    #######
     
     contexto = {"lista": lista_resultado}
-    
     return render(request, 'testCat.html', contexto)
 
